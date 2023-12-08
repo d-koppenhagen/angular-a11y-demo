13c3cbd2639100169a4bdbb8e1a3d6f612f5fee5
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2023-12-08 23:07:13 +0100

    feat: add deferred loading example with aria-live region

--- src/app/home-view/home-view.component.ts
+++ src/app/home-view/home-view.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgxLipsumComponent } from 'ngx-lipsum';
+import { type ILoremIpsumParams } from 'lorem-ipsum';
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
-  imports: [],
+  imports: [NgxLipsumComponent],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.scss'
 })
 export class HomeViewComponent {
+  config: ILoremIpsumParams = { count: 5, units: 'paragraphs' }
 }
